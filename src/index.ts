3cae65e8f029a636da6b18172a96d8030c300463
a-wahab-jafar <a.wahab.jafar1@gmail.com>
2026-02-10 19:41:58 +0300

    implement get all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import subjectsRouter from './routes/subjects';
+import cors from 'cors';
 
 const app: Express = express();
 const PORT: number = 8000;
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
-// Logging middleware
-app.use((req: Request, res: Response, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
-  next();
-});
+app.use(express.json());
+app.use('/api/subjects', subjectsRouter);
+
 
 // Root GET route
 app.get('/', (req: Request, res: Response) => {
